e540e8e7ea3b5a28a1e5ac69776c1e7a2b876113
Ralph van Schaik <ralph.van.schaik@me.com>
2020-11-06 16:24:46 +0100

    TM 131 User choice

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const startGameBtn = document.getElementById('start-game-btn');
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
 
-function startGame() {
-    console.log('Game is starting...');
+let gameIsRunning = false;
+
+const getPlayerChoice = function() {
+    let selection = prompt(`Please choose ${ROCK}, ${PAPER}, ${SCISSORS}`, '').toUpperCase();
+    if (
+        selection !== ROCK &&
+        selection !== PAPER &&
+        selection !== SCISSORS
+    ) {
+        alert(`we choose ${DEFAULT_USER_CHOICE} for you !`);
+        return DEFAULT_USER_CHOICE;
+    }
+    return selection;
+
 }
 
-startGameBtn.addEventListener('click', startGame);
+startGameBtn.addEventListener('click', function() {
+    if (gameIsRunning) {
+        return ;
+    }
+    gameIsRunning = true;
+    console.log('Game is starting...');
+    const playerSelection = getPlayerChoice();
+    console.log(playerSelection);
+});
 
 // startGame();
